18e726d1cb639aefc113eee1c87001e9b5f92d0a
saket suman <12mdpur@gmail.com>
2024-07-16 09:17:40 +0530

    subarray-sum-equals-k

---
+++ 560.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int ans=0;
+        mp[0]=1;
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(mp.find(sum-k)!=mp.end()){
+                ans+=mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
