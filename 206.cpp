f2fcedc223ab59baf53025ed2c2485be96729219
saket suman <12mdpur@gmail.com>
2024-07-19 19:49:35 +0530

    reverse-linked-list

---
+++ 206.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* reverseRecursive(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* newhead=reverseRecursive(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return newhead;
+    }
+
+
+    ListNode* reverseList(ListNode* &head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        return reverseRecursive(head);
+    }
+};
